12f5d175268510b6791678f4e7a944e00639b61a
iColtz <max.hurst@hotmail.com>
2020-12-05 21:49:16 +0000

    HypixelClient(getPlayer): added getPlayer method

--- src/lib/HypixelClient.js
+++ src/lib/HypixelClient.js
@@ -1,8 +1,21 @@
+const fetch = require('node-fetch');
+
 class HypixelClient {
   constructor(key) {
     this.key = key;
 
     this.API = 'https://api.hypixel.net/';
+  }
+
+  async getPlayer(displayName) {
+    try {
+      const data = await fetch(this.API + 'player?key=' + this.key + '&name=' + displayName);
+      const body = await data.json();
+      return body;
+    }
+    catch (error) {
+      console.log(new Error(error));
+    }
   }
 }
 
